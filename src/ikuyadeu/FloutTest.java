e9bb4460fda48dacf573820f3b4a42d3c254de26
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-05-13 16:55:15 +0900

    Set FloutTest's constracta

--- src/ikuyadeu/FloutTest.java
+++ src/ikuyadeu/FloutTest.java
@@ -2,6 +2,18 @@ package ikuyadeu;
 //小数点のあるテスト
 public class FloutTest extends AbstractTest {
 
+	boolean agari;
+
+	public FloutTest(String operator, int digit1, int digit2,int floutnum,
+			boolean amari,boolean agari) {
+		this.operator = operator;
+		this.digit1 = digit1;
+		this.digit2 = digit2;
+		this.floutnum = floutnum;
+		this.amari = amari;
+		this.agari = agari;
+	}
+	
 	@Override
 	public void makeTest() {
 		// TODO 自動生成されたメソッド・スタブ
